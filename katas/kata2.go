0ab2989abdf0eacde2529cca7cf8153abe035f6b
AmirAliEidivandi <amirah648887@gmail.com>
2023-07-06 22:44:53 +0330

    test commit

--- katas/kata2.go
+++ katas/kata2.go
@@ -14,6 +14,8 @@ func tribonacci(signature [3]float64, n int) []float64 {
 func main() {
 	result1 := tribonacci([3]float64{1, 1, 1}, 9)  // result1 will be [1, 1, 1, 3, 5, 9, 17, 31, 57]
 	result2 := tribonacci([3]float64{0, 0, 1}, 10) // result2 will be [0, 0, 1, 1, 2, 4, 7, 13, 24, 44]
+	result3 := tribonacci([3]float64{0, 0, 2}, 15) // result2 will be [0, 0, 1, 1, 2, 4, 7, 13, 24, 44]
 	fmt.Println(result1)
 	fmt.Println(result2)
+	fmt.Println(result3)
 }
